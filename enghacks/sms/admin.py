b9d86419bbb40e5287dfb9528ec9d98ef9737580
Sam Yu <smyu03@gmail.com>
2019-06-22 12:48:26 -0400

    added user and user health info model

--- enghacks/sms/admin.py
+++ enghacks/sms/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from sms.models import (
+    User,
+    UserHealthInformation
+)
 
-# Register your models here.
+class UserHealthInformationInline(admin.TabularInline):
+    model = UserHealthInformation
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'city', 'email')
+    inlines = [UserHealthInformationInline]
+    
+admin.site.register(User, UserAdmin)
