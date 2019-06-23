eca4a19a8e2b7e38dd878ac15f42f1fc61bf80fe
Sam Yu <smyu03@gmail.com>
2019-06-23 03:21:46 -0400

    combine

--- enghacks/backend/views.py
+++ enghacks/backend/views.py
@@ -6,57 +6,19 @@ from django.contrib.auth import get_user_model
 from backend.serializers import UserLoginSerializer
 from rest_framework.response import Response
 from django.conf import settings
-from backend.models import DirectionThread
+from backend.models import DirectionThread, Place
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 from datetime import datetime, timedelta
 import re
 import googlemaps
+from geopy.distance import geodesic
 
-@csrf_exempt
-def sms_response(request):
-    # Start our TwiML response
-    resp = MessagingResponse()
-
-    # Add a text message
-    msg = resp.message("Test message!")
-
-    # Add a picture message
-    msg.media("https://farm8.staticflickr.com/7568/15785724675_999435f19f_k.jpg")
-
-    return HttpResponse(str(resp))
 
 class UserLoginViewSet(viewsets.ModelViewSet):
     User = get_user_model()
     queryset = User.objects.all()
     serializer_class = UserLoginSerializer
-
-def lst_of_directions(origin, destination):
-    directionsObj = gmaps.directions(origin, destination, "walking")
-    # return(directionsObj[0]['overview_polyline']['warnings'])
-    x = (directionsObj[0]['legs'][0]['steps'])
-
-    distance_lst = []
-    for elem in x:
-        distance_lst.append(str(elem['distance']['text']))
-
-    step_lst_html = []
-    for elem in x:
-        step_lst_html.append(str(elem['html_instructions']))
-
-    step_lst = []
-    for elem in step_lst_html:
-        elem = re.sub('<.*?>', ' ', elem)
-        step_lst.append(elem)
-
-    combined_lst = []
-    for index in range(len(step_lst)):
-        distanceStep = step_lst[index] + "(" + distance_lst[index] + ")"
-        combined_lst.append(distanceStep)
-
-    full_string = " --- ".join(combined_lst)
-    intro = "Here are the directions: "
-    return(intro + full_string)
 
 class SMSDirectionsViewSet(viewsets.ModelViewSet):
     User = get_user_model()
@@ -67,20 +29,16 @@ class SMSDirectionsViewSet(viewsets.ModelViewSet):
     default_number = settings.TWILIO_DEFAULT_CALLERID
     client = Client(account_sid, auth_token)
 
-    current_step_options = [
-        ('USER_LOCATION', 'USER_LOCATION'),
-        ('DESTINATION', 'DESTINATION'),
-        ('DEST_CHOICES', 'DEST_CHOICES'),
-        ('IN_TRANSIT', 'IN_TRANSIT'),
-        ('ARRIVED', 'ARRIVED'),
-    ]
     max_time_threshold = timedelta(hours=2)
     gmaps = googlemaps.Client(key=settings.GOOGLE_MAPS_KEY)
 
     #input handler
     def list(self, request):
         from_num = request.query_params.get('From', None)
-        reply_number = self.format_phone(from_num) if from_num else '+14169095217'
+        if from_num:
+            reply_number = self.format_phone(from_num)
+        else:
+            raise('Not SMS')
         request_body = request.query_params.get('Body')
         return_body = ''
         request_user = self.User.objects.get(phone = reply_number)
@@ -89,7 +47,6 @@ class SMSDirectionsViewSet(viewsets.ModelViewSet):
         ).order_by('-date_time')
         latest_thread = user_threads[0] if len(user_threads) else None
 
-        # NO THREAD
         if not latest_thread or latest_thread.current_step == 'ARRIVED':
             latest_thread = self._create_new_thread(request_user)
             return_body = self._current_step_dialog(latest_thread)
@@ -109,18 +66,23 @@ class SMSDirectionsViewSet(viewsets.ModelViewSet):
             return_body = self._current_step_dialog(latest_thread)
             self.send_text(return_body, reply_number)
             latest_thread.increment_step()
+            return Response(request.data)
 
         elif latest_thread.current_step == 'DESTINATION':
-            places_list = self.get_places_list(
-                latest_thread.start_location,
-                request_body,
+            user_location = latest_thread.start_location
+            user_lat, user_lng = self.geocode_address(user_location)
+            places_list = self.get_places_lst(
+                query=request_body,
+                user_lat=user_lat,
+                user_lng=user_lng,
+                radius=5,
+                direction_thread=latest_thread
             )
-            latest_thread.places_list = places_list
-            latest_thread.save()
-            ## some query function here using the input
+            ## ADD PRINT PLACES LIST FUNCTION
             return_body = self._current_step_dialog(latest_thread)
             self.send_text(return_body, reply_number)
             latest_thread.increment_step()
+            return Response(request.data)
 
         elif latest_thread.current_step == 'DEST_CHOICES':
             places_list = latest_thread.places_list
@@ -173,21 +135,17 @@ class SMSDirectionsViewSet(viewsets.ModelViewSet):
             return_body = 'Hello, this is ____. Please text back your location to begin calculating a route.'
         elif (message_thread.current_step == 'USER_LOCATION'):
             return_body = 'Please text back your destination.'
-        elif (message_thread.current_step == 'DESTINATION'):
-            return_body = 'Please text back your destination.'
-        elif (message_thread.current_step == 'DEST_CHOICES'):
-            return_body = 'Here are your options: '
         elif (message_thread.current_step == 'IN_TRANS'):
             return_body = 'Thank you for using ____.'
         return (return_body)
 
     def send_text(self, return_body, reply_number):
-        message = self.client.messages \
-                .create(
-                     body=return_body,
-                     from_=self.default_number,
-                     to=reply_number
-                )
+        self.client.messages \
+            .create(
+                    body=return_body,
+                    from_=self.default_number,
+                    to=reply_number
+            )
 
     def format_phone(self, phone_number):
         # strip non-numeric characters
@@ -228,4 +186,37 @@ class SMSDirectionsViewSet(viewsets.ModelViewSet):
         lat = geocode[0]['geometry']['location']['lat']
         lng = geocode[0]['geometry']['location']['lng']
         return lat, lng
+
+    def calculate_distance(self, lat1, long1, lat2, long2):
+        place1 = (lat1, long1)
+        place2 = (lat2, long2)
+        distance_in_km = round(geodesic(place1, place2).km, 2)
+        if distance_in_km < 1:
+            return str(distance_in_km * 1000) + "m"
+        else:
+            return str(distance_in_km) + " km"
+
+    def get_places_lst(self, query, user_lat, user_lng, radius, direction_thread):
+        location = str(user_lat) + ', ' + str(user_lng)
+        places_result = self.gmaps.places(query, location, radius)
+        places_array = []
+        for place in places_result['results']:
+            address = place['formatted_address']
+            name = place['name']
+            lat = float(place['geometry']['location']['lat'])
+            lng = float(place['geometry']['location']['lng'])
+            distance_str = self.calculate_distance(
+                lat1=user_lat,
+                long1=user_lng,
+                lat2=lat,
+                long2=lng,
+            )
+            new_place = Place.objects.create(
+                address=address,
+                name=name,
+                direction_thread=direction_thread,
+                distance=distance_str,
+            )
+            places_array.append(new_place)
+        return places_array
 
