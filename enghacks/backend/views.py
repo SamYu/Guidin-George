254cedaaf611755deff5dfafa9673cb30d26d9bd
Josiah Parappally <josiahparappally@gmail.com>
2019-06-23 00:57:29 -0400

    directions function

--- enghacks/backend/views.py
+++ enghacks/backend/views.py
@@ -24,3 +24,29 @@ class UserLoginViewSet(viewsets.ModelViewSet):
     User = get_user_model()
     queryset = User.objects.all()
     serializer_class = UserLoginSerializer
+
+def lst_of_directions(origin, destination):
+    directionsObj = gmaps.directions(origin, destination, "walking")
+    # return(directionsObj[0]['overview_polyline']['warnings'])
+    x = (directionsObj[0]['legs'][0]['steps'])
+
+    distance_lst = []
+    for elem in x:
+        distance_lst.append(str(elem['distance']['text']))
+
+    step_lst_html = []
+    for elem in x:
+        step_lst_html.append(str(elem['html_instructions']))
+
+    step_lst = []
+    for elem in step_lst_html:
+        elem = re.sub('<.*?>', ' ', elem)
+        step_lst.append(elem)
+
+    combined_lst = []
+    for index in range(len(step_lst)):
+        distanceStep = step_lst[index] + "(" + distance_lst[index] + ")"
+        combined_lst.append(distanceStep)
+
+    full_string = " --- ".join(combined_lst)
+    return(full_string)
