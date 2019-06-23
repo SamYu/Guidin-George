20bac4be2f3e3d3ce25c9a8adb7496a30096b050
Josiah Parappally <josiahparappally@gmail.com>
2019-06-22 21:13:56 -0400

    Able to print JSON object to console

--- maps/directions.py
+++ maps/directions.py
@@ -1,4 +1,5 @@
 import googlemaps
+import json
 from datetime import datetime
 
 gmaps = googlemaps.Client(key='AIzaSyBadB6G00B0XCY3GoybhFADV9ZnrOP_Usw')
@@ -11,15 +12,23 @@ gmaps = googlemaps.Client(key='AIzaSyBadB6G00B0XCY3GoybhFADV9ZnrOP_Usw')
 
 # Request directions via public transit
 
-origin = "Mavis and Bristol"
-destination = "Credit Valley Hospital"
-mode = "walking"
+# origin = "Mavis and Bristol"
+# destination = "Credit%20Valley%20Hospital"
+# mode = "walking"
+#
+#
+# now = datetime.now()
+# # directions_result = gmaps.directions(origin, destination, mode)
+# directions_result = gmaps.directions("Sydney Town Hall",
+#                                      "Parramatta, NSW",
+#                                      mode="transit",
+#                                      departure_time=now)
+# print(directions_result)
+#
 
+def lst_of_directions(origin, destination):
+    directions_result = gmaps.directions(origin, destination, "walking")
+    pretty_directions_result = json.dumps(directions_result)
+    return pretty_directions_result.type()
 
-now = datetime.now()
-# directions_result = gmaps.directions(origin, destination, mode)
-directions_result = gmaps.directions("Sydney Town Hall",
-                                     "Parramatta, NSW",
-                                     mode="transit",
-                                     departure_time=now)
-print(directions_result)
+print(lst_of_directions("Mavis and Bristol", "Mavis and Eglinton"))
